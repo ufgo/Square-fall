ecb9ef28a92b96d0cef509297f6d35480a9dbf39
Alexander <ufgo009@gmail.com>
2021-05-20 13:43:44 +0600

    some fixes

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 const cachePrefix = "lumber-chop";
 
 // 2. Increment the version every time you publish your game!
-const cacheName = cachePrefix + "-v8";
+const cacheName = cachePrefix + "-v9";
 
 const cacheList = [
     "https://yandex.ru/games/sdk/v2",

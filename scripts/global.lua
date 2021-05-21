a41e9c9ee6014dbec8176dac7c2a0e03ce0bf659
Alexander <ufgo009@gmail.com>
2021-05-21 13:42:28 +0600

    0.1.0

--- scripts/global.lua
+++ scripts/global.lua
@@ -1,6 +1,26 @@
 local M = {}
 
 local isGameOver=false
+local score=0
+local badDomain=false
+
+function M.GetBadDomain()
+	return badDomain
+end
+
+function M.setBadDomain(v)
+	badDomain=v
+end
+
+
+function M.GetScore()
+	return score
+end
+
+function M.SetScore(v)
+	score=v
+end
+
 
 function M.GetGameOver()
 	return isGameOver
